06656fac02299f0416ae41cd9b641b574badc2ef
mhkarimi78 <mahsa_karim16@yahoo.com>
2023-10-26 13:50:00 +0330

    added ui

---
+++ const/addresses.ts
@@ -0,0 +1,6 @@
+export const TRANSFER_CONTRACT_ADDRESS = '0x0FA8313789C27d1e87b93bA2884a095D45766edb';
+export const CALIM_TOKEN_CONTRACT_ADDRESS = '0x6bDc614373A01A38273d2a6647F20bb712879D07';
+
+export const HERO_IMAGE_URL = 'ipfs://QmUWVRuyFuVfo45estsJt2VaQUSEc4uwcPoeWFaeYxERua/seanwatase_isometric_crypto_transferring_from_one_wallet_to_ano_96084ff1-fe03-4dfd-8783-a3ecc85a24da.png';
+export const FEATURES_IMAGE_URL = 'ipfs://QmcTgfb2AXj1TcVj4i9jSgZc8A8LhxHYCXbu5Mngsx7iVT/Coins.png';
+export const CLAIM_TOKEN_IMAGE = 'ipfs://QmUYpRzQE6bebNuuVriyd2n163wAKu1pNRusqQraeRU5bX/seanwatase_isometric_black_crypto_coin_transparent_background_3dcbfe44-babc-45f5-9d43-881d85d2293d.png';
